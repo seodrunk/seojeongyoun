af49250c9daeb02fbd089b0f7e7b1781a5242a56
Seojeong Youn <lyricyoon@gmail.com>
2020-10-06 14:07:16 +0200

    a

--- index.js
+++ index.js
@@ -34,44 +34,54 @@ const sliders = document.querySelectorAll('.project');
 
 
 
-  // how many images to make a gif?
-  var counter = 0;
-  var numImages = 91;
+  // // how many images to make a gif?
+  // var counter = 0;
+  // var numImages = 91;
+  //
+  //
+  // // change this number to make perfect loop gif
+  // var divider = 2000 / numImages;
+  //
+  // $( "body" ).mousemove(function(e) {
+  //
+  //   // convert horizontal position to a number from 0 to 12
+  //   // Y for vertical move, X for horizontal!
+  //   var counter = Math.round(e.pageX / divider);
+  //
+  //   if (counter < 10){
+  //     var imgname = "images/profile/profile_0000" + counter + ".png";
+  //   }
+  //   else {
+  //     var imgname = "images/profile/profile_000" + counter + ".png";
+  //   }
+  //
+  //   $(".profile img").attr("src",imgname);
+  //
+  // });
 
 
-  // change this number to make perfect loop gif
-  var divider = 2000 / numImages;
-
-  $( "body" ).mousemove(function(e) {
-
-    // convert horizontal position to a number from 0 to 12
-    // Y for vertical move, X for horizontal!
-    var counter = Math.round(e.pageX / divider);
-
-    if (counter < 10){
-      var imgname = "images/profile/profile_0000" + counter + ".png";
-    }
-    else {
-      var imgname = "images/profile/profile_000" + counter + ".png";
-    }
-
-    $(".profile img").attr("src",imgname);
-
-  });
+// // activate the close button
+// $(".close-button").click(function(){
+// 	// hide the popup
+// 	$('.popup').hide();
+// });
+//
+// // activate the navigation links
+// $("#bio").click(function(){
+//   // hide popup in case one is open
+//   $('.popup').hide();
+// 	// get the id of the clicked menu item
+// 	var id = $(this).attr('id');
+//   // use it to open the connected popup
+// 	$("#popup-" + id).show();
+// });
 
 
-// activate the close button
-$(".close-button").click(function(){
-	// hide the popup
-	$('.popup').hide();
-});
 
-// activate the navigation links
-$("#bio").click(function(){
-  // hide popup in case one is open
-  $('.popup').hide();
-	// get the id of the clicked menu item
-	var id = $(this).attr('id');
-  // use it to open the connected popup
-	$("#popup-" + id).show();
-});
+function myFunction() {
+  console.log("click");
+var x = document.getElementById("hide");
+if (x.style.display === "none") {
+   x.style.display = "block";}
+  else {x.style.display = "none";}
+          }
